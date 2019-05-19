PUT-PTM/2019_DockerControl

commit 085c6812461bc633ed11f152ceb40e7b4bf0643c

stm | menu flow, sending commands with data

DockerControlSTM/Src/main.c
@@ -96,12 +96,14 @@ extern enum esp_connection_state connection_state;
 
 // dc
 extern enum DC_COMMAND_ENUM cmd;
-extern uint8_t new_containers;
-extern uint8_t containers_size;
-extern struct container containers[20];
-extern uint8_t new_images;
-extern uint8_t images_size;
-extern uint8_t images[20][50];
+extern uint8_t dc_ready;
+extern uint8_t dc_wait;
+extern uint8_t dc_new_containers;
+extern uint8_t dc_containers_size;
+extern struct container dc_containers[];
+extern uint8_t dc_new_images;
+extern uint8_t dc_images_size;
+extern image dc_images[];
 
 // uart
 uint8_t uart_receive[5];
@@ -154,9 +156,9 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     }
 }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-  if(htim->Instance == TIM2){
+  if(htim->Instance == TIM2) {
     if (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_5) == GPIO_PIN_SET) {
         enter_pressed = 1;
     }
@@ -172,21 +174,19 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
     HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
     HAL_TIM_Base_Stop(&htim2);
   }
-  if(htim->Instance == TIM4){
+  if (htim->Instance == TIM4) { // TODO
     HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
     cmd = CALL;
-    dc_make_body(); //TODO: Add parameters (???)
     dc_send(&huart3);
 
     //TODO: check if delay is needed
     cmd = IALL;
-    dc_make_body(); //TODO: Add parameters (???)
     dc_send(&huart3);
   }
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-  if (GPIO_Pin == GPIO_PIN_5 || GPIO_Pin == GPIO_PIN_1 || GPIO_Pin ==  GPIO_PIN_2 || GPIO_Pin ==  GPIO_PIN_3){// && HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_5) == GPIO_PIN_SET) {
+  if (GPIO_Pin == GPIO_PIN_5 || GPIO_Pin == GPIO_PIN_1 || GPIO_Pin ==  GPIO_PIN_2 || GPIO_Pin ==  GPIO_PIN_3) {// && HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_5) == GPIO_PIN_SET) {
     HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
     HAL_TIM_Base_Start_IT(&htim2);
   }
@@ -242,11 +242,8 @@ int main(void)
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
   util_log("DockerControl ready");
 
-  show_menu();
-//  show_containers(containers, &containers_size);
-//  show_containers(); //TODO: move it
+  connection_menu();
   start_dc();
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -255,39 +252,36 @@ int main(void)
 // clion not to alert endless loop
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
-    uint8_t send = 1;
     util_log("endless loop");
     while (1)
     {
-        if (packet_received == 1) {
+        if (dc_ready) {
+            dc_send(&huart3);
+            dc_ready = 0;
+        }
+        else if (packet_received) {
             util_log("got packet");
+            dc_wait = 0;
 
             dc_new_cmd(received_packet_header, received_packet_body);
-            util_log(DC_COMMAND_STRING[cmd]);
-            if (send == 1) {
-                cmd = CALL;
-                dc_send(&huart3);
-                send = 0;
-            }
+
             packet_received = 0;
         }
-        if(usb_received == 1){
+        else if (usb_received) {
             HAL_UART_Transmit_IT(&huart3, usb_data, sizeof(usb_data));
             usb_received = 0;
         }
-        if (new_containers == 1) {
-            show_containers(containers, &containers_size);
-            new_containers = 0;
+        else if (!dc_wait) {
+            main_menu(dc_containers, &dc_containers_size, dc_images, &dc_images_size);
         }
-
 
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
 
-#pragma clang diagnostic pop
-
   }
+
+#pragma clang diagnostic pop
   /* USER CODE END 3 */
 }
 

DockerControlSTM/Src/menu/menu.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 uint8_t esp_param = PARAM_IP;
-uint8_t current_menu = MENU_START;
+
 
 // lcd
 char menu_first_line[48];
@@ -27,17 +27,17 @@ extern UART_HandleTypeDef huart3;
 extern enum DC_COMMAND_ENUM cmd;
 
 // menu
-bool menu_finished = false;
+uint8_t menu_finished = 0;
 
 // buttons
 volatile uint8_t enter_pressed = 0; //encoder
 volatile uint8_t confirm_pressed = 0; //green
 volatile uint8_t back_pressed = 0; //white
 volatile uint8_t shift_pressed = 0; //blue
-bool shift = 0;
+uint8_t shift = 0;
 
-int button_enter(){
-    if(enter_pressed){
+int button_enter() {
+    if(enter_pressed) {
         enter_pressed = 0;
         return 1;
     }
@@ -46,8 +46,8 @@ int button_enter(){
     }
 }
 
-int button_shift(){
-    if(shift_pressed){
+int button_shift() {
+    if(shift_pressed) {
         shift_pressed = 0;
         return 1;
     }
@@ -56,8 +56,8 @@ int button_shift(){
     }
 }
 
-int button_back(){
-    if(back_pressed){
+int button_back() {
+    if(back_pressed) {
         back_pressed = 0;
         return 1;
     }
@@ -66,8 +66,8 @@ int button_back(){
     }
 }
 
-int button_confirm(){
-    if(confirm_pressed){
+int button_confirm() {
+    if(confirm_pressed) {
         confirm_pressed = 0;
         return 1;
     }
@@ -97,7 +97,7 @@ void menu_line(uint8_t line, char *format, ...) {
             update_screen();
         }
     }
-    else if (line == 1){
+    else if (line == 1) {
         if (strcmp(menu_second_line, buffer) != 0) {
             strcpy(menu_second_line, buffer);
             update_screen();
@@ -105,14 +105,14 @@ void menu_line(uint8_t line, char *format, ...) {
     }
 }
 
-void menu_custom(){
+void connection_menu_custom(uint8_t * const current_menu) {
     switch (positions % 5) {
         case 0:
             menu_line(0, ">Server IP: %s", &server_ip);
             menu_line(1, " Server Port: %s", &server_port);
             if (button_enter()) {
                 esp_param = PARAM_IP;
-                current_menu = MENU_CUSTOM_SERVER;
+                *current_menu = MENU_CUSTOM_SERVER;
 
             }
             break;
@@ -121,7 +121,7 @@ void menu_custom(){
             menu_line(1, ">Server Port: %s", &server_port);
             if (button_enter()) {
                 esp_param = PARAM_PORT;
-                current_menu = MENU_CUSTOM_SERVER;
+                *current_menu = MENU_CUSTOM_SERVER;
             }
             break;
         case 2:
@@ -129,7 +129,7 @@ void menu_custom(){
             menu_line(1, " WiFi Password: %s", &wifi_password);
             if (button_enter()) {
                 esp_param = PARAM_NAME;
-                current_menu = MENU_CUSTOM_WIFI;
+                *current_menu = MENU_CUSTOM_WIFI;
             }
             break;
         case 3:
@@ -137,7 +137,7 @@ void menu_custom(){
             menu_line(1, ">WiFi Password: %s", &wifi_password);
             if (button_enter()) {
                 esp_param = PARAM_PASSWORD;
-                current_menu = MENU_CUSTOM_WIFI;
+                *current_menu = MENU_CUSTOM_WIFI;
             }
             break;
         case 4:
@@ -146,27 +146,109 @@ void menu_custom(){
             if (button_enter()) {
                 menu_line(0, "Connecting...");
                 menu_line(1, "");
-                menu_finished = true;
+                menu_finished = 1;
             }
         default:
             break;
     }
 }
 
-void menu_start(){
+void connection_menu_server(uint8_t * const current_menu) {
+
+    uint8_t name[48];
+    volatile uint8_t name_pos = 0;
+    uint8_t character[1] = {0};
+    for(int i=0; i<48; i++) {
+        name[i] = 0;
+    }
+
+    character[0] = (uint8_t) (positions % 10 + 48);
+    char temp = (positions % 10 + 48);
+    menu_line(0, "Number: %c %c >%c< %c %c", temp-2, temp-1, temp, temp+1, temp+2);
+    menu_line(1, name);
+
+    if (button_enter()) {
+        name[name_pos] = character[0];
+        name_pos++;
+    }
+    if (button_shift()) {
+        name[name_pos] = '.';
+        name_pos++;
+    }
+    if (button_back()) {
+        if(name_pos > 0) {
+            name_pos--;
+            name[name_pos] = ' ';
+        }
+    }
+
+    if (button_confirm()) {
+        switch (esp_param) {
+            case PARAM_IP:
+                strcpy(&server_ip, name);
+                break;
+            case PARAM_PORT:
+                strcpy(&server_port, name);
+                break;
+        }
+        *current_menu = MENU_CUSTOM;
+    }
+}
+
+void connection_menu_wifi(uint8_t * const current_menu) {
+    uint8_t name[48];
+    volatile uint8_t name_pos = 0;
+    uint8_t character[1] = {0};
+    for(int i=0; i<48; i++) {
+        name[i] = 0;
+    }
+
+    character[0] = (uint8_t) ((positions % 26 + 65 + shift * 32));
+    char temp2 = (positions % 26 + 65 + shift * 32);
+    menu_line(0, "Letter: %c %c >%c< %c %c  %d", temp2-2, temp2-1, temp2, temp2+1, temp2+2, name_pos);
+    menu_line(1, name);
+
+    if (button_enter()) {
+        name[name_pos] = character[0];
+        name_pos++;
+    }
+    if (button_shift()) {
+        shift = !shift;
+    }
+    if (button_back()) {
+        if(name_pos > 0) {
+            name_pos--;
+            name[name_pos] = ' ';
+        }
+    }
+
+    if (button_confirm()) {
+        switch (esp_param) {
+            case PARAM_NAME:
+                strcpy(&wifi_name, name);
+                break;
+            case PARAM_PASSWORD:
+                strcpy(&wifi_password, name);
+                break;
+        }
+        *current_menu = MENU_CUSTOM;
+    }
+}
+
+void connection_menu_start(uint8_t * const current_menu) {
     switch (positions % 2) {
         case 0:
             menu_line(0, ">Default");
             menu_line(1, " Custom");
             if (button_enter()) {
-                current_menu = MENU_DEFAULT;
+                *current_menu = MENU_DEFAULT;
             }
             break;
         case 1:
             menu_line(0, " Default");
             menu_line(1, ">Custom");
             if (button_enter()) {
-                current_menu = MENU_CUSTOM;
+                *current_menu = MENU_CUSTOM;
             }
             break;
         default:
@@ -174,103 +256,76 @@ void menu_start(){
     }
 }
 
-menu show_menu(){
-    util_log("Show_menu begin");
+void connection_menu() {
+    util_log("connection_menu begin");
+    uint8_t current_menu = MENU_START;
 
-// string
-    uint8_t name[48];
-    volatile uint8_t name_pos = 0;
-    uint8_t character[1] = {0};
-    for(int i=0; i<48; i++){
-        name[i] = 0;
-    }
     while(!menu_finished) {
         pulse_count = (uint8_t) TIM1->CNT;
         positions = (uint8_t) (pulse_count / 4);
         switch (current_menu) {
             case MENU_START:
-                menu_start();
+                connection_menu_start(&current_menu);
                 break;
             case MENU_DEFAULT:
                 menu_line(0, "Connecting using defaults");
                 menu_line(1, "");
-                menu_finished = true;
+                menu_finished = 1;
                 break;
             case MENU_CUSTOM:
-                menu_custom();
+                connection_menu_custom(&current_menu);
                 break;
             case MENU_CUSTOM_SERVER:
-                character[0] = (uint8_t) (positions % 10 + 48);
-                char temp = (positions % 10 + 48);
-                menu_line(0, "Number: %c %c >%c< %c %c", temp-2, temp-1, temp, temp+1, temp+2);
-                menu_line(1, name);
-
-                if (button_enter()) {
-                    name[name_pos] = character[0];
-                    name_pos++;
-                }
-                if (button_shift()) {
-                    name[name_pos] = '.';
-                    name_pos++;
-                }
-                if (button_back()) {
-                    if(name_pos > 0) {
-                        name_pos--;
-                        name[name_pos] = ' ';
-                    }
-                }
-
-                if (button_confirm()) {
-                    switch (esp_param) {
-                        case PARAM_IP:
-                            strcpy(&server_ip, name);
-                            break;
-                        case PARAM_PORT:
-                            strcpy(&server_port, name);
-                            break;
-                    }
-                    current_menu = MENU_CUSTOM;
-                }
+                connection_menu_server(&current_menu);
                 break;
             case MENU_CUSTOM_WIFI:
-                character[0] = (uint8_t) ((positions % 26 + 65 + shift * 32));
-                char temp2 = (positions % 26 + 65 + shift * 32);
-                menu_line(0, "Letter: %c %c >%c< %c %c  %d", temp2-2, temp2-1, temp2, temp2+1, temp2+2, name_pos);
-                menu_line(1, name);
-
-                if (button_enter()) {
-                    name[name_pos] = character[0];
-                    name_pos++;
-                }
-                if (button_shift()) {
-                    shift = !shift;
-                }
-                if (button_back()) {
-                    if(name_pos > 0) {
-                        name_pos--;
-                        name[name_pos] = ' ';
-                    }
-                }
-
-                if (button_confirm()) {
-                    switch (esp_param) {
-                        case PARAM_NAME:
-                            strcpy(&wifi_name, name);
-                            break;
-                        case PARAM_PASSWORD:
-                            strcpy(&wifi_password, name);
-                            break;
-                    }
-                    current_menu = MENU_CUSTOM;
-                }
+                connection_menu_wifi(&current_menu);
+                break;
+            default:
                 break;
         }
     }
 }
 
-void show_containers(const struct container * const containers, const uint8_t * const size) {
-    bool show_details = false;
-    bool show_containers_finished = false;
+void main_menu_start(uint8_t * const current_menu) {
+    switch (positions % 4) {
+        case 0:
+            menu_line(0, ">Containers");
+            menu_line(1, " Images");
+            if (button_enter()) {
+                *current_menu = MAIN_CONTAINERS;
+            }
+            break;
+        case 1:
+            menu_line(0, ">Images");
+            menu_line(1, " Alerts");
+            if (button_enter()) {
+                *current_menu = MAIN_IMAGES;
+            }
+            break;
+        case 2:
+            menu_line(0, ">Alerts");
+            menu_line(1, " Stats");
+            if (button_enter()) {
+                *current_menu = MAIN_ALERTS;
+            }
+            break;
+        case 3:
+            menu_line(0, ">Stats");
+            menu_line(1, " Containers");
+            if (button_enter()) {
+                *current_menu = MAIN_STATS;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+uint8_t main_menu_containers(uint8_t * const current_menu, const struct container * const containers, const uint8_t * const size) {
+    uint8_t show_details = 0;
+    uint8_t show_containers_finished = 0;
+    uint8_t action_performed = 0;
 
     while(!show_containers_finished) {
         pulse_count = (uint8_t) TIM1->CNT;
@@ -288,24 +343,70 @@ void show_containers(const struct container * const containers, const uint8_t * 
         if (button_enter()) {
             show_details = !show_details;
         }
-        if(button_shift()){
-            menu_container_action();
+        if (button_shift()) {
+            action_performed = menu_container_action(&i);
+            if (action_performed) show_containers_finished = 1;
         }
-        if(button_back()){
+        if (button_back()) {
 
         }
-        if(button_confirm()){
-            show_containers_finished = true;
+        if (button_confirm()) {
+            show_containers_finished = 1;
+        }
+    }
+    return action_performed;
+}
+
+void main_menu_images(uint8_t * const current_menu, const image * const images, const uint8_t * const size) {
+
+}
+
+void main_menu_alerts(uint8_t * const current_menu) {
+
+}
+
+void main_menu_stats(uint8_t * const current_menu) {
+
+}
+
+void main_menu(
+    const struct container * const containers, const uint8_t * const containers_size,
+    const image * const images, const uint8_t * const images_size
+) {
+    util_log("main menu begin");
+    uint8_t current_menu = MAIN_START;
+    menu_finished = 0;
+    while (!menu_finished) {
+        pulse_count = (uint8_t) TIM1->CNT;
+        positions = (uint8_t) (pulse_count / 4);
+        switch (current_menu) {
+            case MAIN_START:
+                main_menu_start(&current_menu);
+                break;
+            case MAIN_CONTAINERS:
+                menu_finished = main_menu_containers(&current_menu, containers, containers_size);
+                break;
+            case MAIN_IMAGES:
+                main_menu_images(&current_menu, images, images_size);
+                break;
+            case MAIN_ALERTS:
+                main_menu_alerts(&current_menu);
+                break;
+            case MAIN_STATS:
+                main_menu_stats(&current_menu);
+                break;
+            default:
+                break;
         }
     }
 }
 
-void menu_container_action(){
-    while(!button_enter()){
+uint8_t menu_container_action(const uint8_t * const container_index) {
+    while(!button_enter()) {
         pulse_count = (uint8_t) TIM1->CNT;
         positions = (uint8_t) (pulse_count / 4);
         uint8_t i = positions % 4;
-        switch(i){
+        switch(i) {
             case 0:
                 cmd = CSTR;
                 menu_line(1, ">start restart stop del");
@@ -322,8 +423,13 @@ void menu_container_action(){
                 cmd = CRMV;
                 menu_line(1, " start restart stop>del");
                 break;
+            default:
+                break;
         }
+
+        if (button_back()) return 0;
     }
-    dc_make_body(); // containers[i].id; //TODO: Add parameters
-    dc_send(&huart3);
+    dc_add_data_container(container_index);
+    dc_set_ready();
+    return 1;
 }

DockerControlSTM/Inc/dc/dc.h
@@ -11,12 +11,16 @@ struct container {
     uint8_t status[11];
 };
 
+typedef uint8_t image[51];
+
 void dc_new_cmd(const uint8_t * packet_header, const uint8_t * packet_body);
 void dc_resolve_cmd(const uint8_t * packet_body);
 void dc_apply_cmd(const uint8_t * packet_header, const uint8_t * packet_body);
 
-void dc_make_header();
-void dc_make_body();
+void dc_add_data_container(const uint8_t * container_index);
+void dc_add_data_image(const uint8_t * image_index);
+
+void dc_set_ready();
 
 void dc_start_session(UART_HandleTypeDef * huart);
 
@@ -25,5 +29,6 @@ void dc_send(UART_HandleTypeDef * huart);
 void dc_set_session_id(const uint8_t * packet_header);
 void dc_update_containers(const uint8_t * packet_body);
 void dc_update_images(const uint8_t * packet_body);
+
 
 #endif //DOCKERCONTROLSTM_DC_H

DockerControlSTM/Inc/menu/menu.h
@@ -2,17 +2,16 @@
 #define DOCKERCONTROLSTM_MENU_H
 
 #include "main.h"
-#include <stdbool.h>
-#include<string.h>
+#include <string.h>
 #include "dc/dc.h"
 
-typedef enum{
+typedef enum {
     MENU_START,
     MENU_DEFAULT,
     MENU_CUSTOM,
     MENU_CUSTOM_SERVER,
-    MENU_CUSTOM_WIFI
-} menu;
+    MENU_CUSTOM_WIFI,
+} menu_connection;
 
 typedef enum{
     PARAM_IP,
@@ -21,10 +20,23 @@ typedef enum{
     PARAM_PASSWORD
 } esp_params;
 
-menu show_menu();
-void show_containers(const struct container * containers, const uint8_t * size);
+
+typedef enum {
+    MAIN_START,
+    MAIN_CONTAINERS,
+    MAIN_IMAGES,
+    MAIN_STATS,
+    MAIN_ALERTS
+} menu_main;
+
 void menu_line(uint8_t line, char * format, ...);
 void update_screen();
-void menu_container_action();
+
+void connection_menu();
+
+void main_menu(const struct container * containers, const uint8_t * containers_size,
+               const image * images, const uint8_t * images_size);
+
+uint8_t menu_container_action(const uint8_t * container_index);
 
 #endif

commit fc4ce9fd6043f02bcc8789b6f77f77bbe94a9af9

stm | fixed input bug introduced in 085c6812461bc633ed11f152ceb40e7b4bf0643c

DockerControlSTM/Src/main.c
@@ -243,7 +243,51 @@ int main(void)
   util_log("DockerControl ready");
 
   connection_menu();
-  start_dc();
+//  start_dc();
+
+//mock - ESP problems
+  memcpy(dc_containers[0].id, "42e4da71fbafbfff1c852d3c0460b451d4fd2946625aece758f688f31d3e6666", 65);
+  memcpy(dc_containers[0].name, "TEST_M_DB", 51);
+  memcpy(dc_containers[0].image, "mongo:3.4.10-jessie", 51);
+  memcpy(dc_containers[0].state, "exited", 11);
+  memcpy(dc_containers[0].status, "(0) 9h", 11);
+
+  memcpy(dc_containers[1].id, "6673dfa47a8c3c55e4d6f2522d70ecdf49065862921d60648e7f31ea6d9c0ba7", 65);
+  memcpy(dc_containers[1].name, "TEST_DB", 51);
+  memcpy(dc_containers[1].image, "mongo:3.4.10-jessie", 51);
+  memcpy(dc_containers[1].state, "exited", 11);
+  memcpy(dc_containers[1].status, "(0) 9h", 11);
+
+  memcpy(dc_containers[2].id, "938615e3a6e43724e3d5fa865a1219b74c40e3aee65f854d88897eb6aeacfb26", 65);
+  memcpy(dc_containers[2].name, "portainer-dev", 51);
+  memcpy(dc_containers[2].image, "portainer/portainer", 51);
+  memcpy(dc_containers[2].state, "running", 11);
+  memcpy(dc_containers[2].status, "up 2h", 11);
+
+  memcpy(dc_containers[3].id, "185b63f460a86879b714774377f8db42be9a675c8a6b981a9cf9ed04df5613d1", 65);
+  memcpy(dc_containers[3].name, "priceless_newton", 51);
+  memcpy(dc_containers[3].image, "portainer/portainer", 51);
+  memcpy(dc_containers[3].state, "exited", 11);
+  memcpy(dc_containers[3].status, "(2) 3d", 11);
+
+  memcpy(dc_containers[4].id, "abc3993d342153648adc28d833d973dc3684d2ac45680b13516aaafb952e9da0", 65);
+  memcpy(dc_containers[4].name, "pedantic_mirzakhani", 51);
+  memcpy(dc_containers[4].image, "portainer/portainer", 51);
+  memcpy(dc_containers[4].state, "exited", 11);
+  memcpy(dc_containers[4].status, "(1) 3d;", 11);
+
+
+  memcpy(dc_images[0], "portainer/portainer",50);
+  memcpy(dc_images[1], "mongo:3.4.10-jessie",50);
+  memcpy(dc_images[2], "ubuntu:16.4",50);
+  memcpy(dc_images[3], "portainer/portainer",50);
+
+
+  dc_new_containers = 1;
+  dc_containers_size = 5;
+  dc_images_size = 4;
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */

DockerControlSTM/Src/menu/menu.c
@@ -158,81 +158,87 @@ void connection_menu_server(uint8_t * const current_menu) {
     uint8_t name[48];
     volatile uint8_t name_pos = 0;
     uint8_t character[1] = {0};
+    char temp;
     for(int i=0; i<48; i++) {
         name[i] = 0;
     }
 
-    character[0] = (uint8_t) (positions % 10 + 48);
-    char temp = (positions % 10 + 48);
-    menu_line(0, "Number: %c %c >%c< %c %c", temp-2, temp-1, temp, temp+1, temp+2);
-    menu_line(1, name);
+    while(!button_confirm()) {
+        pulse_count = (uint8_t) TIM1->CNT;
+        positions = (uint8_t) (pulse_count / 4);
+        character[0] = (uint8_t) (positions % 10 + 48);
+        temp = (positions % 10 + 48);
 
-    if (button_enter()) {
-        name[name_pos] = character[0];
-        name_pos++;
-    }
-    if (button_shift()) {
-        name[name_pos] = '.';
-        name_pos++;
-    }
-    if (button_back()) {
-        if(name_pos > 0) {
-            name_pos--;
-            name[name_pos] = ' ';
+        menu_line(0, "Number: %c %c >%c< %c %c", temp - 2, temp - 1, temp, temp + 1, temp + 2);
+        menu_line(1, name);
+
+        if (button_enter()) {
+            name[name_pos] = character[0];
+            name_pos++;
+        }
+        if (button_shift()) {
+            name[name_pos] = '.';
+            name_pos++;
+        }
+        if (button_back()) {
+            if (name_pos > 0) {
+                name_pos--;
+                name[name_pos] = ' ';
+            }
         }
     }
-
-    if (button_confirm()) {
-        switch (esp_param) {
-            case PARAM_IP:
-                strcpy(&server_ip, name);
-                break;
-            case PARAM_PORT:
-                strcpy(&server_port, name);
-                break;
-        }
-        *current_menu = MENU_CUSTOM;
+    switch (esp_param) {
+        case PARAM_IP:
+            strcpy(&server_ip, name);
+            break;
+        case PARAM_PORT:
+            strcpy(&server_port, name);
+            break;
     }
+    *current_menu = MENU_CUSTOM;
 }
 
 void connection_menu_wifi(uint8_t * const current_menu) {
     uint8_t name[48];
     volatile uint8_t name_pos = 0;
     uint8_t character[1] = {0};
+    char temp2;
     for(int i=0; i<48; i++) {
         name[i] = 0;
     }
 
-    character[0] = (uint8_t) ((positions % 26 + 65 + shift * 32));
-    char temp2 = (positions % 26 + 65 + shift * 32);
-    menu_line(0, "Letter: %c %c >%c< %c %c  %d", temp2-2, temp2-1, temp2, temp2+1, temp2+2, name_pos);
-    menu_line(1, name);
+    while(!button_confirm()) {
+        pulse_count = (uint8_t) TIM1->CNT;
+        positions = (uint8_t) (pulse_count / 4);
+        character[0] = (uint8_t) ((positions % 26 + 65 + shift * 32));
+        temp2 = (positions % 26 + 65 + shift * 32);
 
-    if (button_enter()) {
-        name[name_pos] = character[0];
-        name_pos++;
-    }
-    if (button_shift()) {
-        shift = !shift;
-    }
-    if (button_back()) {
-        if(name_pos > 0) {
-            name_pos--;
-            name[name_pos] = ' ';
+        menu_line(0, "Letter: %c %c >%c< %c %c  %d", temp2 - 2, temp2 - 1, temp2, temp2 + 1, temp2 + 2, name_pos);
+        menu_line(1, name);
+
+        if (button_enter()) {
+            name[name_pos] = character[0];
+            name_pos++;
+        }
+        if (button_shift()) {
+            shift = !shift;
+        }
+        if (button_back()) {
+            if (name_pos > 0) {
+                name_pos--;
+                name[name_pos] = ' ';
+            }
         }
     }
-
-    if (button_confirm()) {
-        switch (esp_param) {
-            case PARAM_NAME:
-                strcpy(&wifi_name, name);
-                break;
-            case PARAM_PASSWORD:
-                strcpy(&wifi_password, name);
-                break;
-        }
-        *current_menu = MENU_CUSTOM;
+    switch (esp_param) {
+        case PARAM_NAME:
+            strcpy(&wifi_name, name);
+            break;
+        case PARAM_PASSWORD:
+            strcpy(&wifi_password, name);
+            break;
     }
+    *current_menu = MENU_CUSTOM;
 }
 
 void connection_menu_start(uint8_t * const current_menu) {